22a8ec716db151fdd980d959e443b28d8f24c0b8
Eduardo Vergara <eduver_san@hotmail.com>
2023-01-08 10:34:14 -0500

    style: :lipstick: Update syles responsive

--- components/pokemon/PokemonCard.jsx
+++ components/pokemon/PokemonCard.jsx
@@ -2,19 +2,19 @@ import Image from 'next/image';
 
 export function PokemonCard({name, id, no}) {
   return (
-    <article className='col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 relative mt-6'>
+    <article className='col-span-12 sm:col-span-4 md:col-span-3 lg:col-span-2 relative mt-4 sm:mt-5'>
       <a href='#!' data-mdb-ripple='true' data-mdb-ripple-color='light' className='group'>
-        <div className='rounded-lg shadow-lg bg-white max-w-sm'>
+        <div className='rounded-lg shadow-lg bg-white'>
           <Image
             width={192}
             height={192}
-            priority={false}
+            priority
             alt={name}
-            className='-z-0 aspect-square absolute right-1 -top-8 w-40 group-hover:scale-125 transition-all'
+            className='-z-0 aspect-square absolute right-1 bottom-0 sm:-top-8 w-24 sm:w-36 group-hover:opacity-100 group-hover:scale-110 sm:group-hover:scale-[1.20] transition-all opacity-90'
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${no}.svg`}
           />
-          <div className='p-6 pt-28 z-10'>
-            <h5 className='text-gray-900 z-10 text-xl font-medium uppercase'>{name}</h5>
+          <div className='p-4 sm:pt-28 z-10'>
+            <h2 className='text-gray-900 z-10 text-xl font-medium uppercase'>{name}</h2>
           </div>
         </div>
       </a>
