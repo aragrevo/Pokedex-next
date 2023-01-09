2ff354ab24c6abcfaa38777eb9327a79fa6a7619
Eduardo Vergara <eduver_san@hotmail.com>
2023-01-09 12:38:49 -0500

    feat: :sparkles: Add search

--- components/ui/index.js
+++ components/ui/index.js
@@ -1,2 +1,3 @@
-export {Pagination} from '@components/ui/Pagination';
 export {Input} from '@components/ui/Input';
+export {Pagination} from '@components/ui/Pagination';
+export {SearchInput} from '@components/ui/SearchInput';
